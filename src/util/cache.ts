2827681c3081e320052dca496ccd5e3a2ff3ebfa
Marc Fornos <marc.fornos@gmail.com>
2025-10-29 13:39:48 +0100

    move concurrent creator to util

---
+++ src/util/cache.ts
@@ -0,0 +1,35 @@
+export type Cache<T> = {
+	get(key: string): T | undefined;
+	set(key: string, value: T): void;
+	clear(): void;
+};
+
+export function createCache<T>(maxSize = 1_000): Cache<T> {
+	const cache = new Map<string, T>();
+	let size = 0;
+
+	return {
+		get(key: string): T | undefined {
+			return cache.get(key);
+		},
+
+		set(key: string, value: T): void {
+			if (cache.has(key)) {
+				cache.set(key, value);
+			} else {
+				cache.set(key, value);
+				size++;
+				if (size > maxSize) {
+					const oldestKey = cache.keys().next().value;
+					if (oldestKey !== undefined) cache.delete(oldestKey);
+					size--;
+				}
+			}
+		},
+
+		clear(): void {
+			cache.clear();
+			size = 0;
+		},
+	};
+}
