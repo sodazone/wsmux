fd5ac398affe1a98021e6f4900217c691bf12fef
Marc Fornos <marc.fornos@gmail.com>
2025-11-05 13:04:01 +0100

    log unfollows

--- src/json-rpc/polkadot/chain-head/unfollow.ts
+++ src/json-rpc/polkadot/chain-head/unfollow.ts
@@ -8,8 +8,10 @@ export const chainHead_v1_unfollow: JSONRPCMethodHandler = {
 	async handleRequest(upstream, downstream, req) {
 		const localId = req.params?.[0];
 		if (!localId) return;
+
+		logger.info((l) => l`[${upstream.url}:${localId}] chainHead_v1_unfollow`);
+
 		upstream.unsubscribe(localId);
-		logger.debug((l) => l`Local ${localId} unsubscribed`);
 		downstream.send({ jsonrpc: "2.0", id: req.id ?? null, result: null });
 	},
 };
